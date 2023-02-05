e8c634ac73225e4f4abc8f4d739ab5673abd0062
Ben Roberts <bendando@hotmail.co.uk>
2023-02-05 13:03:57 +0000

    ADD: roadmap page

---
+++ components/Navigation/BasicAppBar.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Link from 'next/link';
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Link href="/">
+            <Typography className='hover' variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Eisenhower Matrix
+            </Typography>
+          </Link>
+          <Link href="/roadmap">
+            <Typography className='hover' variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                RoadMap
+            </Typography>
+          </Link>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ pages/roadmap.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ButtonAppBar from '../components/Navigation/BasicAppBar';
+
+
+
+const RoadMap = () => {
+    return (
+        <>
+        <div className='text-center p-40'>
+            
+            <h1>Road of Project Initially for Quick Developer Reference</h1>
+            <ul>
+                <li><h2>Get the app useful for me ASAP</h2></li>
+                <li>Sort todos and done items</li>
+                <li>Add persistent data</li>
+                <li>Use the persistent data to store information about next jobs for the app</li>
+                <li>Get some drag n drop working to add to eissenhower matrix</li>
+                <li>Create consistend styling</li>
+                <li>Make the app look nice</li>
+                <li>Create and launch app/website</li>
+                <li>Add to app store</li>
+            
+            </ul>
+        </div>
+        </>
+    );
+};
+
+export default RoadMap;
