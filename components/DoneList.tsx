5401b91c5d44aed1669b978fa960c9bed17d33b0
Ben Roberts <bendando@hotmail.co.uk>
2023-05-12 18:14:38 +0100

    ADDED todo item view

--- components/DoneList.tsx
+++ components/DoneList.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import useTodos from '../hooks/useTodos'
-import { Box, Button, Checkbox, List, ListItem, ListItemButton, ListItemText } from '@mui/material'
-import TaskIcon from '@mui/icons-material/Task'
-import DeleteIcon from '@mui/icons-material/Delete'
+import { Box, List, ListItem } from '@mui/material'
 import TodoType from '../types/Todo'
-import useDeleteTodo from '../hooks/useDeleteTodo'
-import useUpdateTodo from '../hooks/useUpdateTodo'
+import TodoItemView from './TodoItemView'
 
 const DoneList = (): JSX.Element => {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const { status, data, error, isFetching } = useTodos()
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const { isLoading, isError, isSuccess, mutate } = useDeleteTodo()
-	const deleteTodo = (id: number): void => { mutate(id) }
-	const { mutate: update } = useUpdateTodo();
+	const { status, data, error, isFetching } = useTodos();
 	const isComplete = (todo: TodoType): boolean => todo.completionDate !== null
-
-	const toggleIsComplete = (todo: TodoType): void => {
-		todo.completionDate = (todo.completionDate == null) ? new Date() : undefined
-		update(todo)
-	}
 
 	return (
 		<div className='p-24'>
@@ -34,14 +21,7 @@ const DoneList = (): JSX.Element => {
 								<List>
 									{data.filter(isComplete).map((item: TodoType) => (
 										<ListItem key={item.id} disablePadding>
-											<ListItemButton>
-												<TaskIcon/>
-												<ListItemText primary={item.name} />
-												<ListItemText primary={item.description} />
-												<Button color="primary" onClick={ () => { deleteTodo(item.id) } } ><DeleteIcon/></Button>
-												{/* <Button onClick={ () => { editTodo(item) } } ><EditIcon/></Button> */}
-												<Checkbox checked={!!item.completionDate} onClick={ () => { toggleIsComplete(item) } } />
-											</ListItemButton>
+											<TodoItemView item={item} />
 										</ListItem>
 									))}
 								</List>
@@ -52,4 +32,4 @@ const DoneList = (): JSX.Element => {
 	)
 }
 
-export default DoneList
+export default DoneList;
--- components/TodoItemView.tsx
+++ components/TodoItemView.tsx
@@ -1,13 +1,42 @@
 
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+import { Button, Checkbox, ListItemButton, ListItemText } from '@mui/material';
 import React from 'react';
+import TaskIcon from '@mui/icons-material/Task'
+import DeleteIcon from '@mui/icons-material/Delete'
+import TodoType from '../types/Todo';
 
-const TodoItemView = (): ReactJSXElement => {
-    return (
+import useDeleteTodo from '../hooks/useDeleteTodo';
+import useUpdateTodo from '../hooks/useUpdateTodo';
+
+interface TodoItemViewProps {
+    item: TodoType
+}
+
+
+const TodoItemView: React.FC<TodoItemViewProps> = ({ item }) => {
+    
+    const { mutate } = useDeleteTodo() 
+	const { mutate: update } = useUpdateTodo()
+	const deleteTodo = (id: number): void => { mutate(id) }
+
+    const toggleIsComplete = (todo: TodoType): void => {
+		todo.completionDate = (todo.completionDate == null) ? new Date() : undefined
+		update(todo)
+	}
+
+    return(
         <>
-            <h1>Create a universal view of item here...</h1>
+            <ListItemButton>
+                <TaskIcon />
+                <ListItemText primary={item.name} />
+                <ListItemText primary={item.description} />
+                <Button color="primary" onClick={() => { deleteTodo(item.id); } }><DeleteIcon /></Button>
+                {/* <Button onClick={ () => { editTodo(item) } } ><EditIcon/></Button> */}
+                <Checkbox checked={!!item.completionDate} onClick={() => { toggleIsComplete(item); } } />
+            </ListItemButton>
         </>
-	);
-};
+    )
+}
 
 export default TodoItemView;
