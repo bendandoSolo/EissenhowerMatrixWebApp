6c2734fad6a2df7f3518f4529e751ca7c0a88d24
Ben Roberts <bendando@hotmail.co.uk>
2023-05-15 17:28:31 +0100

    update roadman plans

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -7,13 +7,14 @@ const RoadMap = (): JSX.Element => (
 			<h1>Road of Project Initially for Quick Developer Reference</h1>
 			<ul>
 				<li><h2>Get the app useful for me ASAP</h2></li>
-				<li>Sort TODOs and done items</li>
 				<li>Get some drag n drop working to add to Eissenhower matrix</li>
 				<li>Add persistent data</li>
+				<li>Add about page about the eissenhower matrix, separate from app page, just using it</li>
 				<li>Use the persistent data to store information about next jobs for the app</li>
 				<li>Create consistent styling</li>
 				<li>Make the app look nice</li>
 				<li>Implement loading spinner when the state is loading</li>
+				<li>Add categories, time estimate, add system to customize those the options you want, add goals linking to categories perhaps</li>
 				<li>Create and launch app/website</li>
 				<li>Add to app store</li>
 			</ul>
